1fad9162d4497d44bf755630ab3c9461d5cf417f
H.U.B. <kunstkr1tik@web.de>
2024-08-06 22:50:00 +0200

    add better inputs for changing grid

--- drawLogic.js
+++ drawLogic.js
@@ -1,22 +1,41 @@
 
 const drawAreaDiv = document.getElementById("drawArea");
 const gridSizeCtl = document.getElementById("gridSizeCtl");
+const gridSizeNumberInput = document.getElementById("gridSizeNumberInput")
 const SCALE = 100;
 
-const COLORS = ["red", "blue", "green", "beige", "purple"];
+const COLORS = ["black", "red", "blue", "green", "beige", "purple"];
+let color = COLORS[0];
+
 
 function pickRandomColor() {
     return COLORS[Math.floor(Math.random() * COLORS.length)];
 }
 
-function initialize() {
+function onChangeNumberInput() {
+    gridSizeCtl.value = gridSizeNumberInput.value;
+    initializeGrid();
+}
+
+function onChangeRangeInput() {
+    gridSizeNumberInput.value = gridSizeCtl.value ;
+    initializeGrid();
+}
+
+// Redraw the grid
+function initializeGrid() {
     let gridSize = gridSizeCtl.value;
     // clean space
     let childrenOfDrawArea = Array.from(drawAreaDiv.children);
     childrenOfDrawArea.forEach((element) => drawAreaDiv.removeChild(element));
 
     // setup divs - square of evenly divided drawAreaDiv.clientWidth)
-    let widthSize = Math.floor((drawAreaDiv.clientWidth / gridSize) * SCALE - 1) / SCALE;
+    let widthSize = drawAreaDiv.clientWidth / gridSize;
+    
+    // Ensure that due to float inprecision, there is no empty column or overdraw
+    if (widthSize != Math.floor(widthSize)) {
+        widthSize = Math.floor(widthSize * SCALE - 1) / SCALE;
+    }
 
     let squareAmount = gridSize * gridSize;
     for (let i = 0; i < squareAmount; i++) {
@@ -29,19 +48,4 @@ function initialize() {
     
 }
 
-//initialize();
-function showAllValues() {
-    gridSizeCtl.value = 1;
-    for (let i = 0; i < 100; i++) {
-        setTimeout(initialize, 1000);
-        setTimeout(null, 0);
-        gridSizeCtl.value = parseInt(gridSizeCtl.value) + 1;
-    }
-}
-
-function addOneMoreGrid() {
-    gridSizeCtl.value = parseInt(gridSizeCtl.value) +1;
-    initialize();
-}
-gridSizeCtl.value = 1;
-//showAllValues();
+initializeGrid();
